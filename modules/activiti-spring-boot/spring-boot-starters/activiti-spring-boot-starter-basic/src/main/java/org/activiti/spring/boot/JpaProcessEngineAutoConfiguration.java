a75d15bd5566dd627f195161c3b1c241f2fdfa92
NiuZian <zian.niu@gdcccn.com>
2021-02-24 10:20:00 +0800

    集成 springboot 1.2.6

--- modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/JpaProcessEngineAutoConfiguration.java
+++ modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/JpaProcessEngineAutoConfiguration.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -11,11 +11,6 @@
  * limitations under the License.
  */
 package org.activiti.spring.boot;
-
-import java.io.IOException;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 import org.activiti.spring.SpringAsyncExecutor;
 import org.activiti.spring.SpringProcessEngineConfiguration;
@@ -30,6 +25,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.io.IOException;
+
 /**
  * @author Joram Barrez
  * @author Josh Long
@@ -39,31 +38,31 @@ import org.springframework.transaction.PlatformTransactionManager;
 @AutoConfigureAfter(DataSourceAutoConfiguration.class)
 public class JpaProcessEngineAutoConfiguration {
 
-  @Configuration
-  @ConditionalOnClass(name= "javax.persistence.EntityManagerFactory")
-  @EnableConfigurationProperties(ActivitiProperties.class)
-  public static class JpaConfiguration extends AbstractProcessEngineAutoConfiguration {
+    @Configuration
+    @ConditionalOnClass(name = "javax.persistence.EntityManagerFactory")
+    @EnableConfigurationProperties(ActivitiProperties.class)
+    public static class JpaConfiguration extends AbstractProcessEngineAutoConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-      return new JpaTransactionManager(emf);
+        @Bean
+        @ConditionalOnMissingBean
+        public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+            return new JpaTransactionManager(emf);
+        }
+
+        @Bean
+        @ConditionalOnMissingBean
+        public SpringProcessEngineConfiguration springProcessEngineConfiguration(
+                DataSource dataSource, EntityManagerFactory entityManagerFactory,
+                PlatformTransactionManager transactionManager, SpringAsyncExecutor springAsyncExecutor) throws IOException {
+
+            SpringProcessEngineConfiguration config = this.baseSpringProcessEngineConfiguration(dataSource,
+                    transactionManager, springAsyncExecutor);
+            config.setJpaEntityManagerFactory(entityManagerFactory);
+            config.setTransactionManager(transactionManager);
+            config.setJpaHandleTransaction(false);
+            config.setJpaCloseEntityManager(false);
+            return config;
+        }
     }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public SpringProcessEngineConfiguration springProcessEngineConfiguration(
-            DataSource dataSource, EntityManagerFactory entityManagerFactory,
-            PlatformTransactionManager transactionManager, SpringAsyncExecutor springAsyncExecutor) throws IOException {
-
-      SpringProcessEngineConfiguration config = this.baseSpringProcessEngineConfiguration(dataSource, 
-          transactionManager, springAsyncExecutor);
-      config.setJpaEntityManagerFactory(entityManagerFactory);
-      config.setTransactionManager(transactionManager);
-      config.setJpaHandleTransaction(false);
-      config.setJpaCloseEntityManager(false);
-      return config;
-    }
-  }
 
 }
---
+++ modules/activiti-ui/activiti-app/src/main/java/org.activiti.app.ui/Application.java
@@ -0,0 +1,11 @@
+package org.activiti.app.ui;
+
+/**
+ * <功能简述>
+ *
+ * @version 1.0 [2021/02/23 16:01]
+ * @author [nza]
+ * @createTime [2021/02/23 16:01]
+ */
+public class Application {
+}
